904ce06aceedbee84d7a22a9e07d736d87fac1e1
Cassycat99 <Cassycat99@yahoo.com>
2019-09-08 10:24:38 -0400

    setting up test canvas

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(800, 800);
+	canvasColor = color(122,244,222);
+	noStroke();
+    background(color(122,244,222)); 
+
+
+    rect(70, 100, 100, 100);
+    rect(210, 100, 100, 100);
+    rect(490,100,240,100);
+    rect(70,600,240,100);
+    fill(255);
+
+    textSize(50);
+    text("Reroll All",508,170);
+
+}
