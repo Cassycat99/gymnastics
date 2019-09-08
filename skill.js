946ba9cf1c4c0833605c06a06d6b41afabd78e1a
Cassycat99 <Cassycat99@yahoo.com>
2019-09-08 11:08:39 -0400

    moving methods

--- skill.js
+++ skill.js
@@ -11,11 +11,30 @@ class Skill {
 
         this.height;
         this.width;
-        
+
         this.fillColor;
 
     }
 
-   // functionGoesHere() { 
-   // }
+   
+addSkill(){
+    fill(this.fillColor);
+    rect(this.x,this.y,this.width,this.height);
+    fill(color(42,104,112));   
+    
+    
+    //add to logic
+    
+}
+	
+eraseSkill(){
+    fill(canvasColor);
+    rect(this.x,this.y,this.width,this.height);
+    fill(42,104,112);
+    rect(70,600,240,100);
+    
+    //take away from logic
+    
+}
+
 }
