e28b713443c9a93510ff0215070a6236977b86f0
Cassycat99 <Cassycat99@yahoo.com>
2019-09-08 10:13:00 -0400

    creating skill object

---
+++ skill.js
@@ -0,0 +1,12 @@
+class Skill {
+    constructor() {
+
+        this.name; //Name of skill
+        this.letter; //A, B, C (D, E, F)
+        this.salto; //Yes/No
+        this.type; //Category (Mount, Leap, Jump, Dismount, etv)
+
+    }
+
+   // functionGoesHere() { 
+   // }
