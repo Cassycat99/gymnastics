6d38314a418e7f77d3575b91f65f5abb6d37ee90
Cassycat99 <Cassycat99@yahoo.com>
2019-09-08 20:25:58 -0400

    adding comments

--- sketch.js
+++ sketch.js
@@ -8,14 +8,15 @@ function setup() {
 
     arrayOfRoutineSkills = [];
 
+    //"library of skills"
     fill(255);
     rect(70, 100, 100, 100);
     fill(color(42,104,112));
     textSize(40);
     text("leap",88,188);
 
-    //build skills[]
-    //display skills
+    //prepopulate library of skills skills[]
+    //draw library of skills
     
 
 
@@ -23,31 +24,24 @@ function setup() {
 
 function mouseClicked() {
 
+    //if mouse is in skill, add skill
     if (mouseX >= 70 && mouseX <= 170 && mouseY >= 100 && mouseY <= 200){
     
         if (arrayOfRoutineSkills.length == 0){
              arrayOfRoutineSkills.push(new Skill("split leap forward","a",0,"leap",50,400,100,100));
-            // arrayOfRoutineSkills.push(skills[]);
+             //arrayOfRoutineSkills.push(libraryOfSkills[]);
             console.log(arrayOfRoutineSkills);
          }
 
          else if (arrayOfRoutineSkills.length == 1){
             arrayOfRoutineSkills.push(new Skill("split leap forward","a",0,"leap",210,400,100,100));
-           // arrayOfRoutineSkills.push(skills[]);
+           // arrayOfRoutineSkills.push(libraryOfSkills[][]);
            console.log(arrayOfRoutineSkills);
         }
         }
          
 
-//adding skill
-
-//check where mouse is
-//set this.x & this.y off that?
-//arrayOfRoutineSkills.push(new Skill(find skill in library?));
-//or change this.x & this.y off mouse here?
-
-
-
+//if mouse is in skill, remove skill
 if (mouseX >= 70 && mouseX <= 170 && mouseY >= 400 && mouseY <= 500){
     
     arrayOfRoutineSkills[0].eraseSkill();
@@ -69,6 +63,15 @@ if (mouseX >= 210 && mouseX <= 310 && mouseY >= 400 && mouseY <= 500){
     arrayOfRoutineSkills.pop();
     console.log(arrayOfRoutineSkills);
 }
+
+
+
+//adding skill
+
+//check where mouse is
+//set this.x & this.y off that?
+//arrayOfRoutineSkills.push(new Skill(find skill in library?));
+//or change this.x & this.y off mouse here?
 
 
 }
